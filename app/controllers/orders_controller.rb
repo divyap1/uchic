e40e105dafbf26e23ac278f483aba09eb40f7c06
Ben <ben.evans_9@hotmail.com>
2016-05-03 09:41:37 +1200

    Removed unnecesary links and cleaned up header/footer sections. Also made it so only the person who placed an order can view that order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,9 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    unless user_signed_in? && @order.buyer_id == current_user.id
+      @order.errors.add(:permission, 'You do not have permission to view this order.')
+    end 
   end
 
   # GET /orders/new
