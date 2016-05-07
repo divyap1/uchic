45dac5557e319ae7f1f94ac994dd17ce876bcf20
Jono Mingard <reason.koan@gmail.com>
2016-05-07 17:16:43 +1200

    Allow users to upload profile pictures

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,10 +6,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :address, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :address, :email, :password, :password_confirmation, :profile_picture)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :address, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :address, :email, :password, :password_confirmation, :current_password, :profile_picture)
   end
 end
