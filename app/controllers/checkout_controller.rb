a8861deb32251ab1086284ab09e1ab3cc70c5892
Divya <1divyap1@gmail.com>
2016-05-10 21:43:39 +1200

    Checkout page shows real product values

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,15 @@
 class CheckoutController < ApplicationController
   def checkout
+    @product = Product.find(params[:id])
+    @shipping = 4.99;
+    @quantity = 1;
+    @subtotal = @product.price * @quantity;
+  end
+
+  #PUT
+  def choose_quantity
+    respond_to do |format|
+
+    end
   end
 end
