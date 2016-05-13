ba2d61713fb931a6a1fcfc879ed3cfb00381ff55
Ben <ben.evans_9@hotmail.com>
2016-05-14 10:27:00 +1200

    Cleaned up following display, and updated any old references to friend_id with following_id

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,14 +3,14 @@ class FriendshipsController < ApplicationController
   # POST /friendships
   # POST /friendships.json
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    @friendship = current_user.friendships.build(:following_id => params[:following_id])
 
     respond_to do |format|
       if @friendship.save
-        format.html { redirect_to user_profile_path(params[:friend_id]), notice: 'Added successfully' }
+        format.html { redirect_to user_profile_path(params[:following_id]), notice: 'Added successfully' }
         format.json { render :show, status: :created, location: @friendship }
       else
-        format.html { redirect_to user_profile_path(params[:friend_id]), alert: 'Unable to add user' }
+        format.html { redirect_to user_profile_path(params[:following_id]), alert: 'Unable to add user' }
         format.json { render :show, status: :created, status: :unprocessable_entity }
       end
     end
@@ -20,11 +20,11 @@ class FriendshipsController < ApplicationController
   # DELETE /friendships/1
   # DELETE /friendships/1.json
   def destroy
-    @friendship = current_user.friendships.where(:friend_id => params[:id]).first
+    @friendship = current_user.friendships.where(:following_id => params[:id]).first
     @friendship.destroy
     
     respond_to do |format|
-      format.html { redirect_to user_profile_path(params[:friend_id]), notice: 'Removed friend.' }
+      format.html { redirect_to user_profile_path(params[:following_id]), notice: 'Removed friend.' }
       format.json { head :no_content }
     end
   end
