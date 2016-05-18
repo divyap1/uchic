5cd690ba0b384bb18496cc843e68c065b8a03f55
hc09141 <hannahcraighead23@gmail.com>
2016-05-18 12:26:12 +1200

    Modified Activity Feed (#40)
    Activity now uses a media list to nicely display notifications. All images
    should have a max height to make all notifications equally sized

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,15 +1,5 @@
 class Commission < ActiveRecord::Base
-  STATES = {
-    "discussion" => "Discussion",
-    "accepted" => "Accepted; awaiting payment",
-    "paid" => "Paid",
-    "in_progress" => "Payment received; in progress",
-    "shipped" => "Shipped"
-  }
-
-  delegate :partner, to: :message_thread
-
-  before_save :reset_acceptance
+  STATES = ["discussion", "accepted", "paid", "in_progress", "shipped"]
 
   belongs_to :seller, class_name: 'User'
   belongs_to :buyer, class_name: 'User'
@@ -18,7 +8,7 @@ class Commission < ActiveRecord::Base
   has_one :message_thread
   has_many :pictures
 
-  validates :state, inclusion: { in: STATES.keys }
+  validates :state, inclusion: { in: STATES }
   validates :seller, presence: true
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }
@@ -28,38 +18,17 @@ class Commission < ActiveRecord::Base
 
   scope :publicly_visible, -> { where(public: true) }
 
-  STATES.each_key.with_index do |state, index|
-    scope state, -> { where(state: state) }
+  STATES.each.with_index do |state, index|
+    matching_states = STATES[index..-1]
 
-    define_method(state + "?") { self.state == state }
-  end
+    scope state, -> { where(state: matching_states) }
 
-  def private?
-    !public?
-  end
-
-  def accepted_by?(user)
-    user == buyer ? accepted_by_buyer? : accepted_by_seller?
-  end
-
-  def accept!(user)
-    if user == buyer
-      update_attributes!(accepted_by_buyer: true)
-    else
-      update_attributes!(accepted_by_seller: true)
-    end
-
-    if accepted_by_buyer? && accepted_by_seller?
-      update_attributes!(state: "accepted")
+    define_method(state + "?") do
+      self.state.in?(matching_states)
     end
   end
 
-  def reset_acceptance
-    unless changed.all? { |a| a == "accepted_by_buyer" || a == "accepted_by_seller" }
-      self.accepted_by_buyer = false
-      self.accepted_by_seller = false
-
-      true # Returning false makes save fail
-    end
+  def editable?
+    discussion? && !accepted_by_seller? && !accepted_by_buyer?
   end
 end
