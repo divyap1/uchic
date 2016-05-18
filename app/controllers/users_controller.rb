0a40293451834487c6b0eac630d97ecc66210b73
Jono Mingard <reason.koan@gmail.com>
2016-05-18 14:32:39 +1200

    Split up public and private commission view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def private_commission
-    @commission = Commissin.find(params[:id])
+    @commission = Commission.find(params[:id])
   end
 
   def edit_private_commission
