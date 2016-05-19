2c7c4a1f11463239a8e82191b27e7688006fb86f
hc09141 <hannahcraighead23@gmail.com>
2016-05-19 17:03:01 +1200

    Review notification (#55)

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -135,6 +135,9 @@ class CommissionsController < ApplicationController
     #Check the current user posted the commission
     if user_signed_in? && (@commission.seller == current_user || @commission.buyer == current_user)
       @commission.destroy!
+      if @commission.buyer
+        @commission.buyer.notifications.create(about_user: current_user, commission: @commission, state: "request denied")
+      end
       respond_to do |format|
         format.html { redirect_to user_dashboard_url, notice: 'Commission was successfully destroyed.'}
         format.json { head :no_content }
