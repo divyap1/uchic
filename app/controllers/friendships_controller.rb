c9c2c6ad15c1dc88a9d668ef61c1d644b2256138
Ben <ben.evans_9@hotmail.com>
2016-05-21 18:39:29 +1200

    Made pagination links display nice (Closes # 74). Aded hovers to notification/messages/profile dropdown. Closable alerts for flash notices also

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,12 +6,13 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(:following_id => params[:following_id])
 
     respond_to do |format|
+      name = User.find_by_id(params[:following_id]).name
       if @friendship.save
-        format.html { redirect_to user_profile_path(params[:following_id]), notice: 'You are now following this user' }
+        format.html { redirect_to user_profile_path(params[:following_id]), notice: "You are now following #{name}" }
         format.json { render :show, status: :created, location: @friendship }
         User.find_by_id(params[:following_id]).notifications.create(about_user_id: current_user.id, state: "new follower");
       else
-        format.html { redirect_to user_profile_path(params[:following_id]), alert: 'Unable to follow user' }
+        format.html { redirect_to user_profile_path(params[:following_id]), alert: "Unable to follow #{name}" }
         format.json { render :show, status: :created, status: :unprocessable_entity }
       end
     end
@@ -25,7 +26,8 @@ class FriendshipsController < ApplicationController
     @friendship.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_profile_path(params[:following_id]), notice: 'You no longer follow this user.' }
+      name = User.find_by_id(params[:following_id]).name
+      format.html { redirect_to user_profile_path(params[:following_id]), notice: "You no longer follow #{name}" }
       format.json { head :no_content }
     end
   end
